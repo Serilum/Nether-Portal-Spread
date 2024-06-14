c7fa20d932d211e7498c081f73d72a8ce6266fd1
Rick South <rick@natamus.com>
2024-06-14 05:21:36 +0200

    Nether Portal Spread source code for 1.21.0.

---
+++ Common/src/main/java/com/natamus/netherportalspread/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.netherportalspread.util;
+
+public class Reference {
+	public static final String MOD_ID = "netherportalspread";
+	public static final String NAME = "Nether Portal Spread";
+	public static final String VERSION = "8.2";
+	public static final String ACCEPTED_VERSIONS = "[1.21.0]";
+}
---
+++ NeoForge/src/main/java/com/natamus/netherportalspread/ModNeoForge.java
@@ -0,0 +1,34 @@
+package com.natamus.netherportalspread;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.netherportalspread.neoforge.config.IntegrateNeoForgeConfig;
+import com.natamus.netherportalspread.neoforge.events.NeoForgeSpreadEvent;
+import com.natamus.netherportalspread.util.Reference;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
+
+@Mod(Reference.MOD_ID)
+public class ModNeoForge {
+	
+	public ModNeoForge(IEventBus modEventBus) {
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		IntegrateNeoForgeConfig.registerScreen(ModLoadingContext.get());
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+		NeoForge.EVENT_BUS.register(NeoForgeSpreadEvent.class);
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
