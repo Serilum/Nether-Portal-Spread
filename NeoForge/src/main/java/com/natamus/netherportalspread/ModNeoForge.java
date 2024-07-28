2ed2366732a15a37c615117854c8325497b13879
Rick South <rick@natamus.com>
2024-07-28 07:24:05 +0200

    (v8.3) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/netherportalspread/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/netherportalspread/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.netherportalspread;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.netherportalspread.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.netherportalspread.neoforge.events.NeoForgeSpreadEvent;
 import com.natamus.netherportalspread.util.Reference;
@@ -14,6 +15,10 @@ import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
