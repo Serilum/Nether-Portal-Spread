15b142dbf0e799dca51df26baec3d08dff130fe2
Rick South <rick@natamus.com>
2026-06-18 23:38:06 +0200

    Updated Nether Portal Spread to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/netherportalspread/util/Reference.java
+++ Common/src/main/java/com/natamus/netherportalspread/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "netherportalspread";
 	public static final String NAME = "Nether Portal Spread";
 	public static final String VERSION = "8.4";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
